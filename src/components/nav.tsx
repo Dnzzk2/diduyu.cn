a8b9281561c67e9f3ce0d40be30e48f2f85d31d4
Dnzzk2 <1933226149@qq.com>
2023-07-15 19:18:58 +0800

    feat(nav.tsx):image

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -24,7 +24,7 @@ const Nav: Component = () => {
       <div class="container mx-auto px-4">
         <div class="flex items-center  justify-between sm:justify-around h-16">
           <div class="flex items-center">
-            <img src="../assets/logoX3.png" alt="logo" width={40} height={40} />
+            <img src="/src/assets/logoX3.png" alt="logo" width={40} height={40} />
             <span class="logo title text-3xl px-1">Du</span>
           </div>
           <div class="text-custom-2 whitespace-nowrap font-alimama  text-2xl  hidden sm:flex">
