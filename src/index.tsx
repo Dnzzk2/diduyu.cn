c86b8eadfee4f14cfe7458d922c4f2f0e8d7c8c0
Dnzzk2 <1933226149@qq.com>
2024-10-08 16:04:36 +0800

    ✨ feat(add loading):

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 /* @refresh reload */
-import './index.css'
-import './assets/font.css'
-import { render } from 'solid-js/web'
+import "./index.css";
+import "./assets/font.css";
+import { render } from "solid-js/web";
 
-import App from './App'
+import App from "./App";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?'
-  )
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
+  );
 }
 
-render(() => <App />, root!)
+// 检测字体加载
+document.fonts.ready.then(() => {
+  document.body.classList.add("fonts-loaded");
+  setTimeout(() => {
+    render(() => <App />, root!);
+  }, 500); // 等待加载动画淡出后再渲染应用
+});
